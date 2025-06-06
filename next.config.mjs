bca858adc3de9cf560a27e6444e2e816a5c540e9
Kirill Korolyov <kirill.korolyov@gmail.com>
2025-06-06 15:13:36 +0700

    Upgrade Node to v22, pnpm to v10, remove bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,16 @@
-import bundleAnalyzer from "@next/bundle-analyzer";
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
+export default withVanillaExtract({
+  ignoreDuringBuilds: true,
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/graphql",
+        destination: process.env.API_URL,
+      },
+    ];
+  },
 });
-
-export default withVanillaExtract(
-  withBundleAnalyzer({
-    ignoreDuringBuilds: true,
-    reactStrictMode: true,
-    async rewrites() {
-      return [
-        {
-          source: "/graphql",
-          destination: process.env.API_URL,
-        },
-      ];
-    },
-  })
-);
