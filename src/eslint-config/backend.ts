784989770c5b5ecd9b623eabf4ea39be51008498
Kirill Korolyov <kirill.korolyov@gmail.com>
2025-07-06 22:57:07 +0700

    Update eslint dependencies and config

--- src/eslint-config/backend.ts
+++ src/eslint-config/backend.ts
@@ -1,26 +1,18 @@
 import eslintPluginNode from "eslint-plugin-n";
 import tseslint from "typescript-eslint";
 
-import { backendFiles } from "./config";
+import { backendFiles, scriptsFiles } from "./config";
 
 export const backend = tseslint.config(
   {
     ...eslintPluginNode.configs["flat/recommended-module"],
-    files: backendFiles,
+    files: [...backendFiles, ...scriptsFiles],
   },
   {
-    files: backendFiles,
+    files: [...backendFiles, ...scriptsFiles],
     name: "my-music/backend",
     rules: {
       "n/no-missing-import": "off",
-      // TODO fix
-      "n/no-extraneous-import": "off",
-      "n/no-unsupported-features/node-builtins": [
-        "error",
-        {
-          version: ">=22.0.0",
-        },
-      ],
       "n/prefer-promises/fs": "error",
       "no-console": "error",
     },
