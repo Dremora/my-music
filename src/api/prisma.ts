d4fd02c2ce8f250c4b245bb1cc9ea8eb9a1525c3
Kirill Korolyov <kirill.korolyov@gmail.com>
2025-08-05 19:03:11 +0700

    Disable Rust Prisma engine

--- src/api/prisma.ts
+++ src/api/prisma.ts
@@ -1,14 +1,23 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+
 import { PrismaClient } from "@/generated/prisma/client";
 
 let prisma: PrismaClient | undefined;
 
 export function getPrismaClient(): PrismaClient {
-  prisma ??= new PrismaClient({
-    log:
-      process.env["NODE_ENV"] === "development"
-        ? ["query", "info", "warn"]
-        : ["warn"],
-  });
+  prisma ??= (() => {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    return new PrismaClient({
+      adapter,
+      log:
+        process.env["NODE_ENV"] === "development"
+          ? ["query", "info", "warn"]
+          : ["warn"],
+    });
+  })();
 
   return prisma;
 }
